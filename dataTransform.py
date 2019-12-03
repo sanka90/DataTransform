6f4f8c56a67d1863a8d75b3b7f646be02f0df9b6
sanka90 <57735144+sanka90@users.noreply.github.com>
2019-12-04 09:17:47 +1100

    This is the 1st version

--- dataTransform.py
+++ dataTransform.py
@@ -1,1 +1,26 @@
-Print("Hi")
+import pandas as pd
+#import pandas_usaddress
+
+class dataTransform:
+  def __init__(self,df):
+    self.df = df
+
+
+  def myfunc(self):
+    # 1st Fill the numerical missing values with the mean of each row
+    self.df = self.df.fillna(self.df.mean())
+    # Impute the categorical with the most frequent value
+    df2 = self.df.apply(lambda x: x.fillna(x.value_counts().index[0]))
+    #Standardize date-time field with a given format
+    df2['Clean_Date'] = df2.Date.apply(lambda x: pd.to_datetime(x).strftime('%d/%m/%Y %H:%M'))
+    #df2.drop(columns=['Name'])
+    #Rounding values as configured
+    df2.round(3)
+    print(df2)
+
+df = pd.read_csv("Pokemon.csv")
+
+print(df)
+p1 = dataTransform(df)
+p1.myfunc()
+
